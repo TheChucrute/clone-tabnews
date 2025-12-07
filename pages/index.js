4d0525d56f8ca01d97f5f87c5d17567afe1627c9
TheChucrute <lucascherutti@gmail.com>
2025-12-07 12:33:45 +0000

    modifica a mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Tefinha, eu, a Anya e a Pluma te amamos! XOXO </h1>
+    return <h1>Tefinha, eu, a Anya e a Pluma te amamos! XOXO ❤️🐈 </h1>
 }
 
 export default Home;
